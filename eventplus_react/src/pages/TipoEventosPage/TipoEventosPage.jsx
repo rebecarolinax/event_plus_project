4b1d407f83730b0565b6551ed468c491771323f2
Rebeca Carolina Fantini Furini De Lima <54931473865@info.local>
2023-11-17 17:18:44 -0300

    :tada: Event Plus

--- eventplus_react/src/pages/TipoEventosPage/TipoEventosPage.jsx
+++ eventplus_react/src/pages/TipoEventosPage/TipoEventosPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./TipoEventosPage.css";
 import Title from "../../components/Title/Title";
 import Container from "../../components/Container/Container";
@@ -10,15 +10,52 @@ import api from "../../Services/Service";
 import { Button, Input } from "../../components/FormComponents/FormComponents";
 import TableEventType from "./TableEventType/TableEventType";
 
+import Notification from "../../components/Notification/Notification";
+
 const TipoEventosPage = () => {
   const [frmEdit, setFrmEdit] = useState(false);
   const [titulo, setTitulo] = useState("");
 
+  const [notifyUser, setNotifyUser] = useState({});
+
   // vai usar um State, sempre tenha um valor inicial.
-  const [tipoEventos, setTipoEventos] = useState([
-    { idTipoEvento: "123", titulo: "AMAR MEU NAMORADO" },
-    { idTipoEvento: "456", titulo: "REBECA CAROLINA" },
-  ]);
+  const [tipoEventos, setTipoEventos] = useState([]);
+
+  // iniciada a lógica de trazer os dados diretamente da API
+  // usando useEffect() para
+  useEffect(() => {
+    // define a chamada em nossa api (chamada assíncrona)
+    async function loadedEventsType() {
+      try {
+        // usando o await para a espera da função "promisse"
+        const retorno = await api.get("/TiposEvento");
+        // instanciando e exibindo os dados
+        setTipoEventos(retorno.data);
+        console.log(retorno.data);
+      } catch (error) {
+        // caso dê erro, exibir no console
+        console.log("Erro na api");
+        console.log(error);
+      }
+    }
+    // chama a função/api
+    loadedEventsType();
+  }, []);
+
+  async function handleDelete(idTipoEvento) {
+    // definido ID e usado como interpolação
+    try {
+      const promise = await api.delete(`${"/TiposEvento"}/${idTipoEvento}`);
+      //  se statusCode == OK
+      if (promise.status == 204) {
+        alert("Cadastro apagado com sucesso!");
+        const buscaEventos = await api.get("/TiposEvento");
+        setTipoEventos(buscaEventos.data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   async function handleSubmit(e) {
     e.preventDefault();
@@ -28,8 +65,18 @@ const TipoEventosPage = () => {
     }
     try {
       const retorno = await api.post("/TiposEvento", { titulo });
-      console.log("Cadastrado com sucesso!");
+
+      setNotifyUser({
+        titleNote: "Sucesso",
+        textNote: `Cadastrado com sucesso!`,
+        imgIcon: "success",
+        imgAlt:
+          "Imagem de ilustração de sucesso. Moça segurando um balão com símbolo de confirmação ok.",
+        showMessage: true,
+      });
+
       console.log(retorno.data);
+
       setTitulo("");
     } catch (error) {
       console.log("Deu ruim na API!");
@@ -38,22 +85,33 @@ const TipoEventosPage = () => {
   }
 
   function showUpdateForm() {
-    alert("Mostrando a tela de Update");
-  }
-
-  function handleUpdate() {
-    alert("Bora atualizar!");
+    setFrmEdit(true);
   }
 
   function editActionAbort() {
-    alert("Cancelar a tela de edição de dados");
+    setFrmEdit(false);
   }
 
-  function handleDelete() {
-    alert("Bora lá apagar da api!");
-  }
+  function handleUpdate() {}
+
+  // async function handleUpdate(idTipoEvento)
+  // {
+  //   // try {
+  //   //   const promise = await api.put(`${"/TiposEvento"}/${idTipoEvento}`);
+  //   //   //  se statusCode == OK
+  //   //   if (promise.status == 204) {
+  //   //     alert("Cadastro atualizado com sucesso!");
+  //   //     const buscaEventos = await api.get("/TiposEvento");
+  //   //     setTipoEventos(buscaEventos.data);
+  //   //   }
+  //   // } catch (error) {
+  //   //   console.log(error);
+  //   // }
+  // }
+
   return (
     <MainContent>
+      <Notification {...notifyUser} setNotifyUser={setNotifyUser} />
       <section className="cadastro-evento-section">
         <Container>
           <div className="cadastro-evento__box">
@@ -77,7 +135,7 @@ const TipoEventosPage = () => {
                       setTitulo(e.target.value);
                     }}
                   />
-                  <span>{titulo}</span>
+
                   <Button
                     type={"submit"}
                     id={"cadastrar"}
@@ -86,7 +144,40 @@ const TipoEventosPage = () => {
                   />
                 </>
               ) : (
-                <p>Tela de edição</p>
+                <>
+                  <Input
+                    type={"text"}
+                    id={"titulo"}
+                    name={"titulo"}
+                    placeholder={"Título"}
+                    required={"required"}
+                    value={titulo}
+                    manipulationFunction={(e) => {
+                      setTitulo(e.target.value);
+                    }}
+                  />
+
+                  <div className="buttons-editbox">
+                    <Button
+                      type={"submit"}
+                      id={"atualizar"}
+                      name={"atualizar"}
+                      textButton={"Atualizar"}
+                      additionalClass={"button-component--middle"}
+                    />
+
+                    <Button
+                      type={"cancel"}
+                      id={"cancelar"}
+                      name={"caneclar"}
+                      textButton={"Cancelar"}
+                      additionalClass={"button-component--middle"}
+                      manipulationFunction={() => {
+                        editActionAbort();
+                      }}
+                    />
+                  </div>
+                </>
               )}
             </form>
           </div>
@@ -95,7 +186,7 @@ const TipoEventosPage = () => {
 
       <section className="lista-eventos-section">
         <Container>
-          <Title titleText={"Lista tipo eventos"} color="white" />
+          <Title titleText={"Listar tipo eventos"} color="white" />
           <TableEventType
             dados={tipoEventos}
             fnUpdate={showUpdateForm}
