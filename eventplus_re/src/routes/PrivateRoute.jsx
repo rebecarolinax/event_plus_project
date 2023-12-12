40de3f2f951ca674d583fa1c8eb8fc749376b3c0
Rebeca Carolina <rebecacarolina75@gmail.com>
2023-12-12 17:15:45 -0300

    Event Plus

--- eventplus_re/src/routes/PrivateRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-export const PrivateRoute = ({children, redirectTo = "/home"}) => {
-    //Verificar se está autenticado
-    const isAuthenticated = localStorage.getItem("token") !== null;
-
-    //Retornar o componente ou navegar para a home
-    return isAuthenticated ? children : <Navigate to={redirectTo}/>
-};
--- eventplus_re/src/utils/stringFunctions.js
+++
@@ -1,4 +0,0 @@
-export const dateFormatDbToView = date => {
-    console.log(date)
-    return new Date(date).toLocaleDateString();
-}
