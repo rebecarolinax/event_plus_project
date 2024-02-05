5c01d3b2bd376059af9ae8f7927a125298cf2f6f
Rebeca <rebecacarolina75@gmail.com>
2024-02-05 15:13:15 -0300

    Event Plus

--- eventplus_react/src/utils/stringFunctions.js
+++
@@ -1,7 +0,0 @@
-export const dateFormatDbToView = data => {
-    //Ex: 2023-09-30T00:00:00 para 30/09/2023
-    data = data.substr(0, 10); //retorna apenas a data (2023-09-30)
-    data = data.split("-"); //[2023, 09, 30]
-
-    return `${data[2]}/${data[1]}/${data[0]}`; //30/09/2023
-}
